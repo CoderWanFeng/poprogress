3270a3c4813f75e6ed38f80b97870c967a5e0359
CoderWanFeng <875143930@qq.com>
2023-03-25 14:42:33 +0800

    update README.md

---
+++ demo/1、simple_progress.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+'''
+@Author  ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
+@WeChat     ：CoderWanFeng
+@Blog      ：www.python-office.com
+@Date    ：2023/3/25 14:40 
+@Description     ：
+'''
+from poprogress import simple_progress
+
+# 平时的代码：
+for i in list:
+    pass
+
+# 加了进度条的代码
+for i in simple_progress(list):
+    pass
